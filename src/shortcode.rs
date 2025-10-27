c6d7b1f2161d6bb2ab615d50cf2134116d7e77ab
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2025-10-27 13:30:55 +0600

    write tests

--- src/shortcode.rs
+++ src/shortcode.rs
@@ -47,3 +47,83 @@ impl<'a> Shortcode<'a> {
         Cow::Owned(Renderer::new(tokens).render(&self.items))
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_shortcode() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |_, _| "Hello world".to_string());
+
+        assert_eq!(shortcode.render("[test]"), "Hello world");
+    }
+
+    #[test]
+    fn test_shortcode_with_content() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |content, _| {
+            format!("T {} T", content.unwrap())
+        });
+        assert_eq!(shortcode.render("[test]Hello world[/test]"), "T Hello world T");
+    }
+
+    #[test]
+    fn test_shortcode_with_attr() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |_, attrs| {
+            format!("T {} T", attrs.get("name").unwrap().unwrap())
+        });
+        assert_eq!(shortcode.render("[test name=\"hello world\"]"), "T hello world T");
+    }
+
+    #[test]
+    fn test_plain_text() {
+        let shortcode = Shortcode::new();
+        assert_eq!(shortcode.render("Hello world"), "Hello world");
+    }
+
+    #[test]
+    fn test_multiple_shortcodes() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |_, _| "Hello world".to_string());
+        shortcode.add("test2", |_, _| "Hello world 2".to_string());
+        assert_eq!(shortcode.render("[test] [test2]"), "Hello world Hello world 2");
+    }
+
+    #[test]
+    fn test_multiple_shortcodes_with_content() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |content, _| {
+            format!("T {} T", content.unwrap())
+        });
+        shortcode.add("test2", |content, _| {
+            format!("T {} T", content.unwrap())
+        });
+        assert_eq!(shortcode.render("[test]Hello world[/test] [test2]Hello world 2[/test2]"), "T Hello world T T Hello world 2 T");
+    }
+
+    #[test]
+    fn test_multiple_shortcodes_with_attr() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |_, attrs| {
+            format!("T {} T", attrs.get("name").unwrap().unwrap())
+        });
+        shortcode.add("test2", |_, attrs| {
+            format!("T {} T", attrs.get("name").unwrap().unwrap())
+        });
+        assert_eq!(shortcode.render("[test name=\"hello world\"] [test2 name=\"hello world 2\"]"), "T hello world T T hello world 2 T");
+    }
+
+    #[test]
+    fn test_nested_shortcodes() {
+        let mut shortcode = Shortcode::new();
+        shortcode.add("test", |_, _| "Hello world".to_string());
+        shortcode.add("test2", |content, _| {
+            format!("T {} T", content.unwrap())
+        });
+        assert_eq!(shortcode.render("[test2][test][/test2]"), "T Hello world T");
+    }
+}
